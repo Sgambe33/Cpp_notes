4396c2e1147a7aa6b97cfc7419feb2d93f098d0f
creeperJam <66206224+creeperJam@users.noreply.github.com>
2024-03-20 22:35:05 +0100

    Update codeeditor.cpp
    Removed line highlight and changed font size

--- codeeditor.cpp
+++ codeeditor.cpp
@@ -61,10 +61,13 @@ CodeEditor::CodeEditor(QWidget *parent) : QPlainTextEdit(parent)
 
     connect(this, &CodeEditor::blockCountChanged, this, &CodeEditor::updateLineNumberAreaWidth);
     connect(this, &CodeEditor::updateRequest, this, &CodeEditor::updateLineNumberArea);
-    connect(this, &CodeEditor::cursorPositionChanged, this, &CodeEditor::highlightCurrentLine);
+    // connect(this, &CodeEditor::cursorPositionChanged, this, &CodeEditor::highlightCurrentLine);
 
     updateLineNumberAreaWidth(0);
-    highlightCurrentLine();
+    QFont font;
+    font.setPointSize(12);
+    this->setFont(font);
+    // highlightCurrentLine();
 }
 
 //![constructor]
